fa1bdbc90ebcbc736b70f7819fe5113c2fdb1948
장현준 <guswnsj0112@naver.com>
2025-07-26 14:01:57 +0000

    changeMineral을 고차함수로 변경 완료 !

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 5173,
-    hmr: {
-      clientPort: 5173,
+    host: '0.0.0.0', // 모든 IP 주소에서 접속 허용
+    port: 3000,     // 구름IDE의 기본 포트 3000으로 설정
+    watch: {
+      usePolling: true, // WSL 또는 일부 가상 환경에서 파일 변경 감지가 안될 때 유용
     },
-
+    // 새로 추가할 설정
     allowedHosts: [
-      'reactvite-jbrys.run.goorm.io', 
+      '.goorm.io', // 구름IDE 도메인 전체를 허용합니다.
+      'minecraft-style-armo-wwfya.run.goorm.io' // 특정 호스트를 명시적으로 추가합니다.
     ],
   },
 });
